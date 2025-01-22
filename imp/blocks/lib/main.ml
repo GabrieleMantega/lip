5ce5467c22777b15dbefad54daa4c283f259c64f
GabrieleMantega <gabrielemantega@gmail.com>
2025-01-22 17:54:24 +0100

    updated imp/blocks: now using NoRuleApplies

--- imp/blocks/lib/main.ml
+++ imp/blocks/lib/main.ml
@@ -67,51 +67,44 @@ let rec eval_decl_list st = function
   | d::l -> eval_decl_list (eval_decl (st) (d)) l 
 ;;
 
-let rec trace1 (st:state) = function
-    Skip -> St st
-  | Assign(x,v) -> 
-    (* TODO: typecheck *)
-    let value = eval_expr st v in
-    let loc = findLoc st x in
-    let st' = 
-      setmem st (
-        bind_mem 
-          (getmem st)
-          (loc_of_envval @@ loc)   
-          value)
-    in St st'
-  | Seq(c1,c2) -> 
-    (let res = trace1 st c1 in
-      match res with
-        St st' -> Cmd(c2,st')
-      | Cmd(c1',st') -> Cmd(Seq(c1',c2),st'))
-  | If(e,c1,c2) -> if (eval_expr st e = Bool true) then Cmd(c1,st) else Cmd(c2,st)
-  | While(e,c) -> if (eval_expr st e = Bool true) then Cmd(Seq(c,While(e,c)), st) else St st
-  | Decl(dl,c) -> 
-    let st' = setenv (st) (bottom_env::getenv st) in
-    let st'' = eval_decl_list (st') dl in Cmd(Block(c),st'')
-  | Block(c) -> Cmd(c, st)
+let rec trace1 = function
+  Cmd(cmd,st) ->
+    (match cmd with
+      Skip -> St st
+    | Assign(x,v) -> 
+      (* TODO: typecheck *)
+      let value = eval_expr st v in
+      let loc = findLoc st x in
+      let st' = 
+        setmem st (
+          bind_mem 
+            (getmem st)
+            (loc_of_envval @@ loc)   
+            value)
+      in St st'
+    | Seq(c1,c2) -> 
+      (let res = trace1 @@ Cmd(c1,st) in
+        match res with
+          St st' -> Cmd(c2,st')
+        | Cmd(c1',st') -> Cmd(Seq(c1',c2),st'))
+    | If(e,c1,c2) -> if (eval_expr st e = Bool true) then Cmd(c1,st) else Cmd(c2,st)
+    | While(e,c) -> if (eval_expr st e = Bool true) then Cmd(Seq(c,While(e,c)), st) else St st
+    | Decl(dl,c) -> 
+      let st' = setenv (st) (bottom_env::getenv st) in
+      let st'' = eval_decl_list (st') dl in Cmd(Block(c),st'')
+    | Block(c) -> Cmd(c, st))
+  | St _ -> raise NoRuleApplies
 ;;
 
 let trace n cmd =
-  let rec rectrace n (cmd:cmd) (st:state) =
-    if n>0 then (
-      let step = trace1 st cmd in
-      match step with
-        St s -> [St s]
-      | Cmd(c,s) -> step::(rectrace (n-1) c s) 
-    )
+  let rec rectrace n conf =
+    if n>0 then
+      try 
+        let step = trace1 conf in
+        conf::(rectrace (n-1) step) 
+      with NoRuleApplies -> [conf]
     else []
   in
 
-  Cmd(cmd,bottom)::(rectrace n cmd bottom)
+  rectrace n (Cmd(cmd,state0))
 ;;
-
-let rec getstate = function
-    [] -> failwith "empty conf-list"
-  | [x] -> 
-    (match x with
-      St st -> st
-      | _ -> failwith "last conf of conf-list is not a state")
-  | _::l -> getstate l
-  ;;
